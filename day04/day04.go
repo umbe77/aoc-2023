7bcdea4cd55d28d20e6c6deee2ad71a1811a19c8
Roberto Ughi <roberto.ughi@apparound.com>
2023-12-04 17:49:19 +0100

    Day 4 Part 2

--- day04/day04.go
+++ day04/day04.go
@@ -10,88 +10,120 @@ import (
 )
 
 func Execute() {
-    cards := make([]Card, 0)
-    utils.ReadFile("day04/input.txt", func(line string) {
-        cards = append(cards, parseLine(line))
-    })
+	cards := make([]*Card, 0)
+	utils.ReadFile("day04/input.txt", func(line string) {
+		cards = append(cards, parseLine(line))
+	})
 
-    fmt.Printf("Part 1: %d\n", part1(cards))
+	fmt.Printf("Part 1: %d\n", part1(cards))
+	fmt.Printf("Part 2: %d\n", part2(cards))
 }
 
 type Card struct {
-    id int
-    wn []int
-    hn []int
+	id   int
+	wins int
+	wn   []int
+	hn   []int
 }
 
-func newCard() Card {
-    return Card{
-        wn: make([]int, 0),
-        hn: make([]int, 0),
-    }
+func newCard() *Card {
+	return &Card{
+		wins: 1,
+		wn:   make([]int, 0),
+		hn:   make([]int, 0),
+	}
 }
 
-func parseLine(row string) Card {
-    card := newCard()
-    r := bufio.NewReader(strings.NewReader(row))
+func parseLine(row string) *Card {
+	card := newCard()
+	r := bufio.NewReader(strings.NewReader(row))
 
-    lastNumRead := 0
-    isReadingW := true
-    for {
-        c, _, err := r.ReadRune()
-        if err != nil {
-            break
-        }
+	lastNumRead := 0
+	isReadingW := true
+	for {
+		c, _, err := r.ReadRune()
+		if err != nil {
+			break
+		}
 
-        if c == ':' {
-            card.id = lastNumRead
-        }
+		if c == ':' {
+			card.id = lastNumRead
+		}
 
-        if c == '|' {
-            isReadingW = false
-        }
+		if c == '|' {
+			isReadingW = false
+		}
 
-        if utils.IsDigit(c) {
-            lastNumRead = utils.ReadInt(r)
-            if card.id != 0 {
-                if isReadingW {
-                    card.wn = append(card.wn, lastNumRead)
-                } else {
-                    card.hn = append(card.hn, lastNumRead)
-                }
-            }
-        }
+		if utils.IsDigit(c) {
+			lastNumRead = utils.ReadInt(r)
+			if card.id != 0 {
+				if isReadingW {
+					card.wn = append(card.wn, lastNumRead)
+				} else {
+					card.hn = append(card.hn, lastNumRead)
+				}
+			}
+		}
 
-    }
+	}
 
-    slices.Sort(card.wn)
+	slices.Sort(card.wn)
 
-    return card
+	return card
 }
 
-func pointPerCard(card Card) int {
-    points := 0
+func pointPerCard(card *Card) int {
+	points := 0
 
-    for _, h := range card.hn {
-        if _, ok := slices.BinarySearch(card.wn, h); ok {
-            points = points + 1
-        }
-    }
+	for _, h := range card.hn {
+		if _, ok := slices.BinarySearch(card.wn, h); ok {
+			points = points + 1
+		}
+	}
 
-    if points == 0 {
-        return points
-    }
-    p := utils.PowInt(2, points-1)
-    return p
+	if points == 0 {
+		return points
+	}
+	p := utils.PowInt(2, points-1)
+	return p
 }
 
-func part1(cards []Card) int {
-    totalPoints := 0
+func part1(cards []*Card) int {
+	totalPoints := 0
 
-    for _, c := range cards {
-        p := pointPerCard(c)
-        totalPoints = totalPoints + p
-    }
+	for _, c := range cards {
+		p := pointPerCard(c)
+		totalPoints = totalPoints + p
+	}
 
-    return totalPoints
+	return totalPoints
+}
+
+func cardsWon(card *Card) int {
+	total := 0
+
+	for _, h := range card.hn {
+		if _, ok := slices.BinarySearch(card.wn, h); ok {
+			total = total + 1
+		}
+	}
+	return total
+}
+
+func part2(cards []*Card) int {
+
+	for i, c := range cards {
+		t := cardsWon(c)
+		for iw := 0; iw < c.wins; iw++ {
+			for j := 1; j <= t && i+j < len(cards); j++ {
+				cards[i+j].wins = cards[i+j].wins + 1
+			}
+		}
+	}
+
+	totalCards := 0
+	for _, c := range cards {
+		totalCards = totalCards + c.wins
+	}
+	return totalCards
 }
--- day04/day04_test.go
+++ day04/day04_test.go
@@ -2,7 +2,6 @@ package day04
 
 import "testing"
 
-func TestPart1(t *testing.T) {
 	var input = []string{
 		"Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
 		"Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
@@ -12,7 +11,8 @@ func TestPart1(t *testing.T) {
 		"Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
 	}
 
-    cards := make([]Card, 0)
+func TestPart1(t *testing.T) {
+    cards := make([]*Card, 0)
     for _, l := range input {
         cards = append(cards, parseLine(l))
     }
@@ -20,5 +20,17 @@ func TestPart1(t *testing.T) {
     r := part1(cards)
     if r != 13 {
         t.Errorf("Expected 13, Got %d", r)
+    }
+}
+
+func TestPart2(t *testing.T) {
+    cards := make([]*Card, 0)
+    for _, l := range input {
+        cards = append(cards, parseLine(l))
+    }
+
+    r := part2(cards)
+    if r != 30 {
+        t.Errorf("Expected 30, Got %d", r)
     }
 }
